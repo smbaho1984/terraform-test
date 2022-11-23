87baab04e94d57e67db2ce8fafd0944fa9dfe7b3
unknown <stephanembaho@gmail.com>
2022-11-23 15:43:52 -0500

    test1

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    default = "ami-08e2d37b6a0129927"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "sg_id" {
+    default = "sg-08c5d7076572d3d26"
+}
